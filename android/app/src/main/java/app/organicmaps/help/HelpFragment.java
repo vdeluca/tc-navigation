bd1b6d90caa8bdf85adb97d4a23f3476b978152c
Vicente Deluca <deluca.vicente@gmail.com>
2024-06-06 20:47:52 -0300

    Modified About Us!

--- android/app/src/main/java/app/organicmaps/help/HelpFragment.java
+++ android/app/src/main/java/app/organicmaps/help/HelpFragment.java
@@ -47,40 +47,11 @@ public class HelpFragment extends BaseMwmFragment implements View.OnClickListene
     final boolean isLandscape = getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE;
 
     final String dataVersion = DateUtils.getShortDateFormatter().format(Framework.getDataVersion());
-    final TextView osmPresentationView = root.findViewById(R.id.osm_presentation);
-    if (osmPresentationView != null)
-      osmPresentationView.setText(getString(R.string.osm_presentation, dataVersion));
 
-    setupItem(R.id.news, true, root);
     setupItem(R.id.web, true, root);
     setupItem(R.id.email, true, root);
-    setupItem(R.id.github, true, root);
-    setupItem(R.id.telegram, false, root);
-    setupItem(R.id.instagram, false, root);
-    setupItem(R.id.facebook, false, root);
-    setupItem(R.id.twitter, true, root);
-    setupItem(R.id.matrix, true, root);
-    setupItem(R.id.mastodon, false, root);
-    setupItem(R.id.openstreetmap, true, root);
-    setupItem(R.id.faq, true, root);
-    setupItem(R.id.report, isLandscape, root);
 
-    final TextView supportUsView = root.findViewById(R.id.support_us);
-    if (BuildConfig.FLAVOR.equals("google") && !TextUtils.isEmpty(mDonateUrl))
-      supportUsView.setVisibility(View.GONE);
-    else
-      setupItem(R.id.support_us, true, root);
 
-    final TextView donateView = root.findViewById(R.id.donate);
-    if (TextUtils.isEmpty(mDonateUrl))
-      donateView.setVisibility(View.GONE);
-    else
-    {
-      if (Config.isNY())
-        donateView.setCompoundDrawablesRelativeWithIntrinsicBounds(R.drawable.ic_christmas_tree, 0,
-            R.drawable.ic_christmas_tree, 0);
-      setupItem(R.id.donate, isLandscape, root);
-    }
 
     if (BuildConfig.REVIEW_URL.isEmpty())
       root.findViewById(R.id.rate).setVisibility(View.GONE);
@@ -102,34 +73,8 @@ public class HelpFragment extends BaseMwmFragment implements View.OnClickListene
     final int id = v.getId();
     if (id == R.id.web)
       Utils.openUrl(requireActivity(), getResources().getString(R.string.translated_om_site_url));
-    else if (id == R.id.news)
-      Utils.openUrl(requireActivity(), getResources().getString(R.string.translated_om_site_url) + "news/");
     else if (id == R.id.email)
       Utils.sendTo(requireContext(), BuildConfig.SUPPORT_MAIL, "Organic Maps");
-    else if (id == R.id.github)
-      Utils.openUrl(requireActivity(), Constants.Url.GITHUB);
-    else if (id == R.id.telegram)
-      Utils.openUrl(requireActivity(), getString(R.string.telegram_url));
-    else if (id == R.id.instagram)
-      Utils.openUrl(requireActivity(), getString(R.string.instagram_url));
-    else if (id == R.id.facebook)
-      Utils.showFacebookPage(requireActivity());
-    else if (id == R.id.twitter)
-      Utils.openUrl(requireActivity(), Constants.Url.TWITTER);
-    else if (id == R.id.matrix)
-      Utils.openUrl(requireActivity(), Constants.Url.MATRIX);
-    else if (id == R.id.mastodon)
-      Utils.openUrl(requireActivity(), Constants.Url.MASTODON);
-    else if (id == R.id.openstreetmap)
-      Utils.openUrl(requireActivity(), getString(R.string.osm_wiki_about_url));
-    else if (id == R.id.faq)
-      ((HelpActivity) requireActivity()).stackFragment(FaqFragment.class, getString(R.string.faq), null);
-    else if (id == R.id.report)
-      Utils.sendBugReport(requireActivity(), "", "");
-    else if (id == R.id.support_us)
-      Utils.openUrl(requireActivity(), getResources().getString(R.string.translated_om_site_url) + "support-us/");
-    else if (id == R.id.donate)
-      Utils.openUrl(requireActivity(), mDonateUrl);
     else if (id == R.id.rate)
       Utils.openAppInMarket(requireActivity(), BuildConfig.REVIEW_URL);
     else if (id == R.id.copyright)
